065d69e82d0b79e472335bbc9272f3b0f5a298d8
shihui <shihui_2021@163.com>
2024-01-05 14:53:48 +0800

    mappings custom

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -37,7 +37,7 @@ M.comment = {
 
   -- toggle comment in both modes
   n = {
-    ["<C>/"] = {
+    ["<C-/>"] = {
       function()
         require("Comment.api").toggle.linewise.current()
       end,
@@ -49,6 +49,112 @@ M.comment = {
     ["<C>/"] = {
       "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
       "Toggle comment",
+    },
+  },
+}
+
+M.lspconfig = {
+  plugin = true,
+  -- See `<cmd> :help vim.lsp.*` for documentation on any of the below functions
+  n = {
+    -- ["gD"] = {
+    ["<leader>dl"] = {
+      function()
+        vim.lsp.buf.declaration()
+      end,
+      "LSP declaration",
+    },
+
+    -- ["gd"] = {
+    ["<leader>df"] = {
+      function()
+        vim.lsp.buf.definition()
+      end,
+      "LSP definition",
+    },
+
+    -- ["K"] = {
+    ["<leader>gi"] = {
+      function()
+        vim.lsp.buf.hover()
+      end,
+      "LSP hover",
+    },
+
+    -- ["gi"] = {
+    ["<leader>im"] = {
+      function()
+        vim.lsp.buf.implementation()
+      end,
+      "LSP implementation",
+    },
+
+    ["<leader>ls"] = {
+      function()
+        vim.lsp.buf.signature_help()
+      end,
+      "LSP signature help",
+    },
+
+    -- ["<leader>D"] = {
+    ["<leader>tdf"] = {
+      function()
+        vim.lsp.buf.type_definition()
+      end,
+      "LSP definition type",
+    },
+
+    -- ["<leader>ra"] = {
+    ["<leader>re"] = {
+      function()
+        require("nvchad.renamer").open()
+      end,
+      "LSP rename",
+    },
+
+    ["<leader>ca"] = {
+      function()
+        vim.lsp.buf.code_action()
+      end,
+      "LSP code action",
+    },
+
+    -- ["gr"] = {
+    ["<leader>rf"] = {
+      function()
+        vim.lsp.buf.references()
+      end,
+      "LSP references",
+    },
+
+    ["<leader>lf"] = {
+      function()
+        vim.diagnostic.open_float { border = "rounded" }
+      end,
+      "Floating diagnostic",
+    },
+
+    -- ["[d"] = {
+    ["<leader>pe"] = {
+      function()
+        vim.diagnostic.goto_prev { float = { border = "rounded" } }
+      end,
+      "Goto prev",
+    },
+
+    -- ["]d"] = {
+    ["<leader>ne"] = {
+      function()
+        vim.diagnostic.goto_next { float = { border = "rounded" } }
+      end,
+      "Goto next",
+    },
+
+    ["<leader>q"] = {
+      function()
+        vim.diagnostic.setloclist()
+      end,
+      "Diagnostic setloclist",
     },
   },
 }
