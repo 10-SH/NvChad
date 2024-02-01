3cd8d72190d96bc6ab70a2b81467d22d1c07fb5f
shihui <shihui_2021@163.com>
2024-02-01 17:48:34 +0800

    theme

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "github_dark",
+  theme = "github_light",
   theme_toggle = { "github_dark", "github_light" },
 
   hl_override = highlights.override,
