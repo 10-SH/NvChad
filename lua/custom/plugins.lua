30e13c431d1ed6a26bf394603fba0bd84fd7527a
shihui <shihui_2021@163.com>
2024-03-13 12:18:04 +0800

    modify

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -220,35 +220,23 @@ local plugins = {
     "nvim-treesitter/nvim-treesitter-refactor",
     lazy = false,
     config = function()
-      require("nvim-treesitter.configs").setup {
-        refactor = {
-          highlight_definitions = {
-            enable = true,
-            -- Set to false if you have an `updatetime` of ~100.
-            clear_on_cursor_move = true,
-          },
-          highlight_current_scope = { enable = true },
-          -- smart_rename = {
-          --   enable = true,
-          --   -- Assign keymaps to false to disable them, e.g. `smart_rename = false`.
-          --   keymaps = {
-          --     smart_rename = "grr",
-          --   },
-          -- },
-          -- navigation = {
-          --   enable = true,
-          --   -- Assign keymaps to false to disable them, e.g. `goto_definition = false`.
-          --   keymaps = {
-          --     goto_definition = "gnd",
-          --     list_definitions = "gnD",
-          --     list_definitions_toc = "gO",
-          --     goto_next_usage = "<a-*>",
-          --     goto_previous_usage = "<a-#>",
-          --   },
-          -- },
-        },
-      }
+      require("nvim-treesitter.configs").setup(overrides.refactor())
     end,
+  },
+
+  {
+    "andrewferrier/wrapping.nvim",
+    lazy = false,
+    config = function()
+      require("wrapping").setup()
+    end,
+  },
+
+  {
+    -- 只显示当前方法 command: Twilight 
+    "folke/twilight.nvim",
+    lazy = false,
+    opts = {},
   },
 
   -- ===设置某个插件不加载=== === === === ===
